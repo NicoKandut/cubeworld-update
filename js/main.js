1f18b8b3b0406d7c79a57861b395429c934ebe92
NicoKandut <35682881+NicoKandut@users.noreply.github.com>
2018-08-14 20:31:08 +0100

    initial release

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const lastUpdateOutput = document.getElementById('last-update');
+    const timeOutput = document.getElementById('time');
+
+    const lastUpdate = moment('2013-07-23');
+
+    lastUpdateOutput.innerText = lastUpdate.format('MMMM Do YYYY');
+
+    setInterval(function () {
+        let now = moment(new Date());
+        let difference = now.diff(lastUpdate);
+        let duration = moment.duration(difference);
+        timeOutput.innerText = 
+            duration.years() + ' years, ' +
+            duration.months() + ' months, ' +
+            duration.days() + ' days, ' +
+            duration.hours() + ' hours, ' +
+            duration.minutes() + ' minutes and ' +
+            duration.seconds() + ' seconds';
+    }, 1000);
+});
