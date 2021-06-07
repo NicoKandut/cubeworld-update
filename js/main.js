6062d5f0644e12934a66fa470a2ed62fa1af2bca
Nico Kandut <nico.kandut@gmail.com>
2021-06-07 18:21:13 +0200

    Updated release date
    closes #1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const lastUpdateOutput = document.getElementById('last-update');
-    const timeOutput = document.getElementById('time');
+document.addEventListener("DOMContentLoaded", function () {
+  const lastUpdateOutput = document.getElementById("last-update");
+  const timeOutput = document.getElementById("time");
 
-    const lastUpdate = moment('2019-09-29');
+  const lastUpdate = moment("2019-09-30");
 
-    lastUpdateOutput.innerText = lastUpdate.format('MMMM Do YYYY');
+  lastUpdateOutput.innerText = lastUpdate.format("MMMM Do YYYY");
 
-    setInterval(function () {
-        let now = moment(new Date());
-        let difference = now.diff(lastUpdate);
-        let duration = moment.duration(difference);
-        timeOutput.innerText = 
-            duration.years() + ' years, ' +
-            duration.months() + ' months, ' +
-            duration.days() + ' days, ' +
-            duration.hours() + ' hours, ' +
-            duration.minutes() + ' minutes and ' +
-            duration.seconds() + ' seconds';
-    }, 1000);
+  setInterval(function () {
+    let now = moment(new Date());
+    let difference = now.diff(lastUpdate);
+    let duration = moment.duration(difference);
+    timeOutput.innerText =
+      duration.years() +
+      " years, " +
+      duration.months() +
+      " months, " +
+      duration.days() +
+      " days, " +
+      duration.hours() +
+      " hours, " +
+      duration.minutes() +
+      " minutes and " +
+      duration.seconds() +
+      " seconds";
+  }, 1000);
 });
